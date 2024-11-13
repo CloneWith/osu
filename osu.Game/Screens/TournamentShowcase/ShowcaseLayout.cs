0e45ce4c3aea4d0a67122a752f7094d587b9ed5b
CloneWith <clonewith@qq.com>
2024-11-13 09:13:59 +0800

    Add showcase UI layout enumerable
    For future use!

---
+++ osu.Game/Screens/TournamentShowcase/ShowcaseLayout.cs
@@ -0,0 +1,17 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace osu.Game.Screens.TournamentShowcase
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ShowcaseLayout
+    {
+        MainScreenOnly,
+        SimpleControl,
+        DetailedControl,
+        Immersive,
+    }
+}
