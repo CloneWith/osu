0d9ed53de944eb9559318186dc2b7e7e91f08524
CloneWith <clonewith@qq.com>
2024-11-19 01:05:11 +0800

    Add exception handling for import errors

--- osu.Game/Screens/TournamentShowcase/ShowcaseStorage.cs
+++ osu.Game/Screens/TournamentShowcase/ShowcaseStorage.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
@@ -31,8 +32,16 @@ namespace osu.Game.Screens.TournamentShowcase
             using (Stream stream = storage.GetStream(name, FileAccess.Read, FileMode.Open))
             using (var sr = new StreamReader(stream))
             {
-                // TODO: Need an async Task?
-                return JsonConvert.DeserializeObject<ShowcaseConfig>(sr.ReadToEnd()) ?? new ShowcaseConfig();
+                try
+                {
+                    // TODO: Need an async Task?
+                    return JsonConvert.DeserializeObject<ShowcaseConfig>(sr.ReadToEnd()) ?? new ShowcaseConfig();
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e, $"Failed to load showcase profile {name}, reverting to default.");
+                    return new ShowcaseConfig();
+                }
             }
         }
 
