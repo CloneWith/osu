b067e470415c99fbf167314d99d4d4fff15652fc
CloneWith <clonewith@qq.com>
2025-01-07 23:27:43 +0800

    Add description as dropdown entries for ShowcaseLayout

--- osu.Game/Screens/TournamentShowcase/ShowcaseLayout.cs
+++ osu.Game/Screens/TournamentShowcase/ShowcaseLayout.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.ComponentModel;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
@@ -9,8 +10,13 @@ namespace osu.Game.Screens.TournamentShowcase
     [JsonConverter(typeof(StringEnumConverter))]
     public enum ShowcaseLayout
     {
+        [Description("Immersive Mode (Fullscreen)")]
         Immersive,
+
+        [Description("Simple Control")]
         SimpleControl,
+
+        [Description("Detailed Control")]
         DetailedControl,
     }
 }
