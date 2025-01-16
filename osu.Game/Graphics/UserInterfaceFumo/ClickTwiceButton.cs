64d78498c64da9988f0dc0ea2c7e9bfe18cadcc1
CloneWith <clonewith@qq.com>
2025-01-16 22:45:30 +0800

    Add Fumo buttons!
    These buttons can be used in showcase controls and the tournament client.

---
+++ osu.Game/Graphics/UserInterfaceFumo/ClickTwiceButton.cs
@@ -0,0 +1,84 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Input.Events;
+
+namespace osu.Game.Graphics.UserInterfaceFumo
+{
+    public partial class ClickTwiceButton : FumoButton
+    {
+        private double? activateTime;
+
+        public string Text
+        {
+            get => activateTime != null ? ActiveText : IdleText;
+            set => IdleText = ActiveText = value;
+        }
+
+        public ClickTwiceButton(FillDirection direction = FillDirection.Horizontal)
+            : base(direction)
+        {
+        }
+
+        protected override void LoadComplete()
+        {
+            Enabled.BindValueChanged(e =>
+            {
+                if (!e.NewValue && activateTime != null) deactivate();
+            });
+
+            base.LoadComplete();
+        }
+
+        protected override void Update()
+        {
+            if (activateTime != null && Clock.CurrentTime - activateTime > 5000)
+            {
+                activateTime = null;
+                deactivate();
+            }
+
+            base.Update();
+        }
+
+        protected override bool OnClick(ClickEvent e)
+        {
+            if (!Enabled.Value) return base.OnClick(e);
+
+            if (activateTime == null)
+            {
+                activateTime = Clock.CurrentTime;
+                activate();
+            }
+            else
+            {
+                Action?.Invoke();
+                activateTime = null;
+                deactivate();
+            }
+
+            return base.OnClick(e);
+        }
+
+        private void activate()
+        {
+            IconSprite.Icon = ActiveIcon ?? IdleIcon ?? new IconUsage();
+            TextSprite.Text = ActiveText != string.Empty ? ActiveText : IdleText;
+            Background.FadeColour(ActiveBackgroundColour, TRANSFORM_DURATION, Easing.OutQuint);
+            IconSprite.FadeColour(ActiveForegroundColour, TRANSFORM_DURATION, Easing.OutQuint);
+            TextSprite.FadeColour(ActiveForegroundColour, TRANSFORM_DURATION, Easing.OutQuint);
+        }
+
+        private void deactivate()
+        {
+            IconSprite.Icon = IdleIcon ?? new IconUsage();
+            TextSprite.Text = IdleText;
+            Background.FadeColour(IdleBackgroundColour, TRANSFORM_DURATION, Easing.OutQuint);
+            IconSprite.FadeColour(IdleForegroundColour, TRANSFORM_DURATION, Easing.OutQuint);
+            TextSprite.FadeColour(IdleForegroundColour, TRANSFORM_DURATION, Easing.OutQuint);
+        }
+    }
+}
---
+++ osu.Game/Graphics/UserInterfaceFumo/FumoButton.cs
@@ -0,0 +1,95 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Graphics.UserInterface;
+using osuTK;
+using osuTK.Graphics;
+
+namespace osu.Game.Graphics.UserInterfaceFumo
+{
+    /// <summary>
+    /// A general button class for those with two statuses and optional icons and texts.
+    /// </summary>
+    public partial class FumoButton : OsuAnimatedButton
+    {
+        protected const int TRANSFORM_DURATION = 600;
+
+        protected readonly FillDirection LayoutDirection;
+
+        protected Box Background = null!;
+        protected SpriteIcon IconSprite = null!;
+        protected SpriteText TextSprite = null!;
+
+        public string IdleText = string.Empty;
+        public string ActiveText = string.Empty;
+        public IconUsage? IdleIcon;
+        public IconUsage? ActiveIcon;
+
+        public ColourInfo IdleBackgroundColour = Color4Extensions.FromHex("#232323");
+        public ColourInfo IdleForegroundColour = Color4.White;
+        public ColourInfo ActiveBackgroundColour = Color4.SkyBlue;
+        public ColourInfo ActiveForegroundColour = Color4.Black;
+
+        public FumoButton(FillDirection direction = FillDirection.Horizontal)
+        {
+            LayoutDirection = direction;
+            AutoSizeAxes = Axes.Both;
+            AutoSizeDuration = TRANSFORM_DURATION;
+            AutoSizeEasing = Easing.OutQuint;
+
+            if (ActiveText == string.Empty) ActiveText = IdleText;
+            ActiveIcon ??= IdleIcon;
+        }
+
+        [BackgroundDependencyLoader]
+        protected void Load()
+        {
+            Content.AddRange(new Drawable[]
+            {
+                Background = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Depth = float.MaxValue,
+                    Colour = IdleBackgroundColour
+                },
+                new FillFlowContainer
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    AutoSizeAxes = Axes.Both,
+                    AutoSizeDuration = TRANSFORM_DURATION,
+                    AutoSizeEasing = Easing.OutQuint,
+                    Direction = LayoutDirection,
+                    Spacing = new Vector2(3),
+                    Margin = new MarginPadding { Horizontal = 8, Vertical = 6 },
+                    Children = new Drawable[]
+                    {
+                        IconSprite = new SpriteIcon
+                        {
+                            Anchor = LayoutDirection == FillDirection.Horizontal ? Anchor.CentreLeft : Anchor.Centre,
+                            Origin = LayoutDirection == FillDirection.Horizontal ? Anchor.CentreLeft : Anchor.Centre,
+                            Icon = IdleIcon ?? new IconUsage(),
+                            Size = new Vector2(20)
+                        },
+                        TextSprite = new OsuSpriteText
+                        {
+                            Anchor = LayoutDirection == FillDirection.Horizontal ? Anchor.CentreLeft : Anchor.Centre,
+                            Origin = LayoutDirection == FillDirection.Horizontal ? Anchor.CentreLeft : Anchor.Centre,
+                            Text = IdleText,
+                            Colour = IdleForegroundColour,
+                            Font = OsuFont.Torus.With(weight: FontWeight.SemiBold)
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
