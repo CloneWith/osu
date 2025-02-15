44eede12f17ae654df18fa0894d2e925d6cd182b
CloneWith <clonewith@qq.com>
2025-02-15 22:59:04 +0800

    Extend countdown placeholder component logic

--- osu.Game.Tournament/Components/MatchCountdown.cs
+++ osu.Game.Tournament/Components/MatchCountdown.cs
@@ -32,6 +32,12 @@ namespace osu.Game.Tournament.Components
         public Color4 NormalContentColour = Color4.Black;
         public Color4 AccentContentColour = Color4.White;
 
+        private const string long_waiting_string = @"还要等很久呢...";
+        private const string empty_time_string = @"在等着什么呢 >.<";
+        private const string ended_string = @"开赛啦 开赛啦！";
+        private const string just_ended_string = @"前就开始了";
+        private const string long_ended_string = @"早就开始啦 >.<";
+
         private readonly Container topLayer;
         private readonly Container bottomLayer;
 
@@ -42,6 +48,7 @@ namespace osu.Game.Tournament.Components
         // Elements specifically used for cases with target time unset.
         private readonly SpriteIcon hourglassIcon;
         private readonly OsuSpriteText waitingText;
+        private readonly OsuSpriteText longCountdownText;
 
         public MatchCountdown()
         {
@@ -116,63 +123,131 @@ namespace osu.Game.Tournament.Components
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Font = OsuFont.Torus.With(size: 32, weight: FontWeight.SemiBold),
-                Text = "Please wait...",
+                Text = empty_time_string,
+                Colour = NormalContentColour,
+            };
+
+            longCountdownText = new OsuSpriteText
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Font = OsuFont.Torus.With(size: 32, weight: FontWeight.SemiBold),
+                Text = long_waiting_string,
                 Colour = NormalContentColour,
             };
         }
+
+        /// <summary>
+        /// If the countdown takes a long time. In this case it means more than one day.
+        /// </summary>
+        /// <param name="timeOffset">a <see cref="DateTimeOffset"/> representing the target time</param>
+        /// <returns>true if taking more than one day, otherwise false.</returns>
+        private bool countdownLongProgress(DateTimeOffset timeOffset) =>
+            timeOffset.ToLocalTime() - DateTimeOffset.Now >= TimeSpan.FromDays(1);
+
+        /// <summary>
+        /// If the countdown just ended, meaning the target time is earlier than current time by less than 3 minutes.
+        /// </summary>
+        /// <param name="timeOffset">a <see cref="DateTimeOffset"/> representing the target time</param>
+        /// <returns>true if just ended, otherwise false.</returns>
+        private bool countdownJustEnded(DateTimeOffset timeOffset) =>
+            countdownEnded(timeOffset) && DateTimeOffset.Now - timeOffset.ToLocalTime() <= TimeSpan.FromMinutes(3);
+
+        private string getWaitingString() => Target.Value.HasValue
+            ? countdownJustEnded(Target.Value.Value)
+                ? @$"{(Target.Value.Value - DateTimeOffset.Now).ToHumanizedString()}{just_ended_string}"
+                : long_ended_string
+            : empty_time_string;
+
+        /// <summary>
+        /// If the countdown is ended.
+        /// </summary>
+        /// <param name="timeOffset">a <see cref="DateTimeOffset"/> representing the target time</param>
+        /// <returns>true if the countdown is ended, otherwise false.</returns>
+        private bool countdownEnded(DateTimeOffset timeOffset) => DateTimeOffset.Now - timeOffset.ToLocalTime() >= TimeSpan.Zero;
 
         protected override void LoadComplete()
         {
             base.LoadComplete();
 
             // TODO: Add code logic for real countdown
-            if (!Target.Value.HasValue) fillPlaceholderContent();
+            if (!Target.Value.HasValue || countdownEnded(Target.Value.Value)) fillPlaceholderContent();
+            else fillTimerContent();
 
             Target.BindValueChanged(targetTimeChanged);
         }
 
         private void targetTimeChanged(ValueChangedEvent<DateTimeOffset?> target)
         {
-            if (target.NewValue == null) fillPlaceholderContent();
+            if (target.NewValue == null || countdownEnded(target.NewValue.Value)) fillPlaceholderContent();
+            else fillTimerContent();
+        }
+
+        private void fillTimerContent()
+        {
+            OnGoing = true;
+
+            contentFlow.FadeOut(150, Easing.OutQuint)
+                       .Delay(300).FadeIn();
+
+            Scheduler.AddDelayed(contentFlow.Clear, false, 300);
         }
 
         private void fillPlaceholderContent()
         {
-            contentFlow.FadeOut(150, Easing.OutQuint)
-                       .Delay(300).FadeIn();
-
-            Scheduler.AddDelayed(_ =>
+            // No need to fade the whole content when countdown state is unchanged
+            if (OnGoing || !Target.Value.HasValue)
             {
-                // Keep children for reusing purposes
-                contentFlow.Clear(false);
+                contentFlow.FadeOut(150, Easing.OutQuint)
+                           .Delay(300).FadeIn();
 
-                hourglassIcon.FadeOut();
-                waitingText.FadeOut();
-                hourglassIcon.ClearTransforms();
-                hourglassIcon.RotateTo(0);
-                waitingText.ClearTransforms();
-            }, false, 150);
-
-            Scheduler.AddDelayed(_ =>
-            {
-                contentFlow.Add(hourglassIcon);
-                hourglassIcon.ScaleTo(2.5f).Then().ScaleTo(1, 500, Easing.OutQuint);
-                hourglassIcon.Delay(100).FadeIn(300, Easing.OutQuint);
-            }, false, 500);
-
-            Scheduler.AddDelayed(_ =>
-            {
-                // Add the placeholder text after a little delay to make it look better
-                contentFlow.Add(waitingText);
-                waitingText.Delay(200).FadeIn(500, Easing.OutQuint);
-
-                using (BeginDelayedSequence(1000))
+                Scheduler.AddDelayed(() =>
                 {
-                    hourglassIcon.RotateTo(0)
-                                 .Then().RotateTo(360 * 5, 3000, Easing.InOutQuint)
-                                 .Loop(3000);
-                }
-            }, false, 1000);
+                    // Keep children for reusing purposes
+                    contentFlow.Clear(false);
+
+                    hourglassIcon.FadeOut();
+                    waitingText.FadeOut();
+                    hourglassIcon.ClearTransforms();
+                    hourglassIcon.RotateTo(0);
+                    waitingText.ClearTransforms();
+
+                    waitingText.Text = getWaitingString();
+                }, 150);
+
+                Scheduler.AddDelayed(() =>
+                {
+                    contentFlow.Add(hourglassIcon);
+                    hourglassIcon.ScaleTo(2.5f).Then().ScaleTo(1, 500, Easing.OutQuint);
+                    hourglassIcon.Delay(100).FadeIn(300, Easing.OutQuint);
+                }, 500);
+
+                Scheduler.AddDelayed(() =>
+                {
+                    // Add the placeholder text after a little delay to make it look better
+                    contentFlow.Add(waitingText);
+                    waitingText.Delay(200).FadeIn(500, Easing.OutQuint);
+
+                    using (BeginDelayedSequence(1000))
+                    {
+                        hourglassIcon.RotateTo(0)
+                                     .Then().RotateTo(360 * 5, 3000, Easing.InOutQuint)
+                                     .Loop(3000);
+                    }
+                }, 1000);
+            }
+            else
+            {
+                waitingText.FadeOut(150, Easing.OutQuint);
+
+                Scheduler.AddDelayed(() =>
+                {
+                    waitingText.Text = getWaitingString();
+                    waitingText.FadeIn(150, Easing.OutQuint);
+                }, 150);
+            }
+
+            OnGoing = false;
         }
     }
 }
---
+++ osu.Game.Tournament/TimeExtensions.cs
@@ -0,0 +1,38 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Globalization;
+using Humanizer;
+using Humanizer.Localisation;
+
+namespace osu.Game.Tournament
+{
+    /// <summary>
+    /// Utilities for time manipulation, which is especially useful in scheduling.
+    /// </summary>
+    public static class TimeExtensions
+    {
+        /// <summary>
+        /// Turn a <see cref="TimeSpan"/> struct into a string representation respecting current culture.
+        /// </summary>
+        /// <param name="timeSpan">the <see cref="TimeSpan"/> to be handled</param>
+        /// <param name="useChineseCharacters">force to use Chinese representation of time (e.g. 1 minute -> 1 分钟)</param>
+        /// <returns>a string representation of the time</returns>
+        /// <remarks>Since Humanizer won't return fully translated time strings, we need to add an argument to handle this by ourselves.</remarks>
+        public static string ToHumanizedString(this TimeSpan timeSpan, bool useChineseCharacters = true)
+        {
+            string humanizedTime = timeSpan.Humanize(culture: CultureInfo.CurrentUICulture, maxUnit: TimeUnit.Year, minUnit: TimeUnit.Second);
+
+            if (useChineseCharacters)
+            {
+                return humanizedTime.Replace("seconds", "second").Replace("minutes", "minute").Replace("seconds", "second").Replace("hours", "hour")
+                                    .Replace("days", "day").Replace("months", "month").Replace("weeks", "week").Replace("years", "year")
+                                    .Replace("second", "秒").Replace("minute", "分钟").Replace("hour", "小时").Replace("day", "天")
+                                    .Replace("week", "周").Replace("year", "年");
+            }
+
+            return humanizedTime;
+        }
+    }
+}
