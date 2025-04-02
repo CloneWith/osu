76474edd75ee827ad7da2621d6ea87e0c1586ac2
CloneWith <clonewith@qq.com>
2025-04-02 21:30:08 +0800

    Add status update and restore methods to ChessPlacement

--- osu.Game.Tournament/Models/ChessPlacement.cs
+++ osu.Game.Tournament/Models/ChessPlacement.cs
@@ -14,11 +14,19 @@ namespace osu.Game.Tournament.Models
     [Serializable]
     public class ChessPlacement
     {
+        /// <summary>
+        /// The team holding the chess right now, representing by a <see cref="TeamColour"/>.
+        /// </summary>
+        /// <remarks>This property isn't supposed to set directly. Use <see cref="Update"/> and <see cref="Undo"/> instead.</remarks>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-        public TeamColour OwnerTeam;
+        public TeamColour OwnerTeam { get; private set; }
 
+        /// <summary>
+        /// The current <see cref="ChoiceType"/> of the chess.
+        /// </summary>
+        /// <remarks>This property isn't supposed to set directly. Use <see cref="Update"/> and <see cref="Undo"/> instead.</remarks>
         [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-        public ChoiceType CurrentType;
+        public ChoiceType CurrentType { get; private set; }
 
         /// <summary>
         /// The ID of the beatmap the chess is associated with. Zero if it isn't associated with one.
@@ -31,7 +39,7 @@ namespace osu.Game.Tournament.Models
         /// <summary>
         /// A list of changes of this chess piece for easier backtracking.
         /// </summary>
-        public List<ChessRecord> Records = new List<ChessRecord>();
+        public readonly List<ChessRecord> Records = new List<ChessRecord>();
 
         public ChessPlacement(int boardRow, int boardColumn, TeamColour ownerTeam = TeamColour.Neutral, ChoiceType type = ChoiceType.Neutral, int beatmapID = 0)
         {
@@ -46,6 +54,44 @@ namespace osu.Game.Tournament.Models
             BoardColumn = boardColumn;
 
             Records.Add(new ChessRecord(OwnerTeam, CurrentType));
+        }
+
+        /// <summary>
+        /// Update the chess's status and record it to the <see cref="Records"/> list.
+        /// </summary>
+        /// <param name="newOwner">the new <see cref="TeamColour"/> of the team holding the chess.</param>
+        /// <param name="newType">the new <see cref="ChoiceType"/> of the chess.</param>
+        public void Update(TeamColour? newOwner, ChoiceType? newType)
+        {
+            if (newOwner == null && newType == null) return;
+
+            if (newOwner != null)
+                OwnerTeam = newOwner.Value;
+
+            if (newType != null)
+                CurrentType = newType.Value;
+
+            Records.Add(new ChessRecord(OwnerTeam, CurrentType));
+        }
+
+        /// <summary>
+        /// Restore the last status of the chess from the <see cref="Records"/> list.
+        /// </summary>
+        /// <returns>true if succeeded, false otherwise (no status to restore from)</returns>
+        public bool Undo()
+        {
+            // Don't undo when only one record is found.
+            if (Records.Count <= 1) return false;
+
+            // Get and restore the last state.
+            var lastRecord = Records[^2];
+
+            OwnerTeam = lastRecord.Colour;
+            CurrentType = lastRecord.NewType;
+
+            // We don't need the latest record anymore.
+            Records.RemoveAt(Records.Count - 1);
+            return true;
         }
     }
 }
