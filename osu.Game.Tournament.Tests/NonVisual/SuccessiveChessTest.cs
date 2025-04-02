e241d49d439cd367b43558068aaa6d4d95755d35
CloneWith <clonewith@qq.com>
2025-04-02 21:30:40 +0800

    Add comprehensive board simulation test

--- osu.Game.Tournament.Tests/NonVisual/SuccessiveChessTest.cs
+++ osu.Game.Tournament.Tests/NonVisual/SuccessiveChessTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using NUnit.Framework;
 using osu.Game.Extensions;
 using osu.Game.Tournament.Models;
@@ -13,20 +14,34 @@ namespace osu.Game.Tournament.Tests.NonVisual
         private readonly TournamentMatch match = new TournamentMatch();
         private (int red, int blue) nums = (0, 0);
 
-        private void runTest(ChessPlacement[] chessList, bool clearList = true)
+        private void runOnce() => nums = match.GetMaximumSuccessiveChess();
+
+        private void runWith(ChessPlacement[] chessList, bool clearList = true)
         {
             if (clearList)
                 match.ChessPlacements.Clear();
 
             match.ChessPlacements.AddRange(chessList);
 
-            nums = match.GetMaximumSuccessiveChess();
+            runOnce();
+        }
+
+        private void updateStatusAt(int row, int col, TeamColour? newOwner, ChoiceType? newType)
+        {
+            var source = match.ChessPlacements.First(r => r.BoardRow == row && r.BoardColumn == col);
+            source.Update(newOwner, newType);
+        }
+
+        private void undoAt(int row, int col)
+        {
+            var source = match.ChessPlacements.First(r => r.BoardRow == row && r.BoardColumn == col);
+            source.Undo();
         }
 
         [Test]
         public void TestEmptyBoardChess()
         {
-            runTest([]);
+            runWith([]);
 
             Assert.AreEqual(0, nums.red, "Empty board test failed: Red chess");
             Assert.AreEqual(0, nums.blue, "Empty board test failed: Blue chess");
@@ -35,7 +50,7 @@ namespace osu.Game.Tournament.Tests.NonVisual
         [Test]
         public void TestSingleWinColourChess()
         {
-            runTest(new[]
+            runWith(new[]
             {
                 new ChessPlacement(1, 1, type: ChoiceType.RedWin),
                 new ChessPlacement(2, 1, type: ChoiceType.RedWin),
@@ -43,7 +58,7 @@ namespace osu.Game.Tournament.Tests.NonVisual
 
             Assert.AreEqual(2, nums.red, "Dual vertical match test failed: Red chess");
 
-            runTest(new[]
+            runWith(new[]
             {
                 new ChessPlacement(1, 1, type: ChoiceType.RedWin),
                 new ChessPlacement(2, 2, type: ChoiceType.RedWin),
@@ -51,7 +66,7 @@ namespace osu.Game.Tournament.Tests.NonVisual
 
             Assert.AreEqual(0, nums.red, "Dual diagonal match test failed: Red chess");
 
-            runTest(new[]
+            runWith(new[]
             {
                 new ChessPlacement(2, 1, type: ChoiceType.RedWin),
                 new ChessPlacement(2, 3, type: ChoiceType.RedWin),
@@ -59,7 +74,7 @@ namespace osu.Game.Tournament.Tests.NonVisual
 
             Assert.AreEqual(3, nums.red, "Vertical dual + horizontal triple matches test failed: Red chess");
 
-            runTest(new[]
+            runWith(new[]
             {
                 new ChessPlacement(1, 4, type: ChoiceType.RedWin),
                 new ChessPlacement(3, 2, type: ChoiceType.RedWin),
@@ -72,7 +87,7 @@ namespace osu.Game.Tournament.Tests.NonVisual
         [Test]
         public void TestDualWinColourChess()
         {
-            runTest(new[]
+            runWith(new[]
             {
                 new ChessPlacement(1, 1, type: ChoiceType.RedWin),
                 new ChessPlacement(1, 2, type: ChoiceType.RedWin),
@@ -83,7 +98,7 @@ namespace osu.Game.Tournament.Tests.NonVisual
             Assert.AreEqual(2, nums.red, "Horizontal dual match test failed: Red chess");
             Assert.AreEqual(0, nums.blue, "Diagonal dual match test failed: Blue chess");
 
-            runTest(new[]
+            runWith(new[]
             {
                 new ChessPlacement(3, 4, type: ChoiceType.BlueWin),
                 new ChessPlacement(4, 4, type: ChoiceType.BlueWin),
@@ -91,7 +106,7 @@ namespace osu.Game.Tournament.Tests.NonVisual
 
             Assert.AreEqual(2, nums.blue, "Dual vertical + diagonal match test failed: Blue chess");
 
-            runTest(new[]
+            runWith(new[]
             {
                 new ChessPlacement(1, 3, type: ChoiceType.RedWin),
                 new ChessPlacement(2, 3, type: ChoiceType.BlueWin),
@@ -100,6 +115,58 @@ namespace osu.Game.Tournament.Tests.NonVisual
 
             Assert.AreEqual(3, nums.red, "Horizontal triple match test failed: Red chess");
             Assert.AreEqual(3, nums.blue, "Triple vertical + diagonal match test failed: Blue chess");
+        }
+
+        [Test]
+        public void TestComprehensiveBoard()
+        {
+            runWith(new[]
+            {
+                new ChessPlacement(1, 3, TeamColour.Red, ChoiceType.Pick),
+                new ChessPlacement(3, 2, TeamColour.Blue, ChoiceType.Pick),
+                new ChessPlacement(1, 4, TeamColour.Red),
+            });
+
+            Assert.AreEqual(0, nums.red, "Match without win types test failed: Red chess");
+            Assert.AreEqual(0, nums.blue, "Initialization test failed: Blue chess");
+
+            updateStatusAt(1, 3, null, ChoiceType.RedWin);
+            updateStatusAt(1, 4, null, ChoiceType.RedWin);
+            updateStatusAt(3, 2, null, ChoiceType.BlueWin);
+            runOnce();
+
+            Assert.AreEqual(2, nums.red, "Update status test failed: Red chess");
+
+            runWith(new[]
+            {
+                new ChessPlacement(2, 4, TeamColour.Red, ChoiceType.RedWin),
+                new ChessPlacement(3, 4, TeamColour.Blue, ChoiceType.RedWin),
+                new ChessPlacement(4, 4, TeamColour.Red, ChoiceType.RedWin),
+            }, false);
+
+            Assert.AreEqual(4, nums.red, "Vertical quad match test failed: Red chess");
+
+            updateStatusAt(2, 4, null, ChoiceType.Consumed);
+            updateStatusAt(3, 4, null, ChoiceType.Consumed);
+            runOnce();
+
+            Assert.AreEqual(2, nums.red, "Chess consumption test failed: Red chess");
+
+            runWith(new[]
+            {
+                new ChessPlacement(1, 2, TeamColour.Red, ChoiceType.RedWin),
+                new ChessPlacement(2, 2, TeamColour.Blue, ChoiceType.BlueWin),
+                new ChessPlacement(4, 2, TeamColour.Blue, ChoiceType.BlueWin),
+            }, false);
+
+            Assert.AreEqual(3, nums.red, "Horizontal triple match test failed: Red chess");
+            Assert.AreEqual(3, nums.blue, "Vertical triple match test failed: Blue chess");
+
+            updateStatusAt(1, 2, null, ChoiceType.BlueWin);
+            undoAt(1, 2);
+            runOnce();
+
+            Assert.AreEqual(3, nums.red, "Chess status (1, 2) restore test failed");
         }
     }
 }
