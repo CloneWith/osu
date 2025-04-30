3344e122a7e4d1bf943ce44bde1be04b09ee6022
CloneWith <clonewith@qq.com>
2025-04-30 22:55:28 +0800

    Pass MouseDown event through FumoChessPiece

--- osu.Game.Tournament/Components/FumoChessPiece.cs
+++ osu.Game.Tournament/Components/FumoChessPiece.cs
@@ -9,6 +9,7 @@ using osu.Framework.Graphics.Effects;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
+using osu.Framework.Input.Events;
 using osu.Game.Graphics.Backgrounds;
 using osu.Game.Graphics.UserInterfaceFumo;
 using osu.Game.Tournament.Models;
@@ -277,5 +278,7 @@ namespace osu.Game.Tournament.Components
 
             topIcon.ScaleTo(1.5f).Then().ScaleTo(1, 500, Easing.OutQuint);
         }
+
+        protected override bool OnMouseDown(MouseDownEvent e) => false;
     }
 }
