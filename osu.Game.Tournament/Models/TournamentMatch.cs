b1ea7a071f02a45040e4f09fdb4df16c4a7ef908
CloneWith <clonewith@qq.com>
2025-05-11 23:41:22 +0800

    Add round count property to TournamentMatch

--- osu.Game.Tournament/Models/TournamentMatch.cs
+++ osu.Game.Tournament/Models/TournamentMatch.cs
@@ -70,6 +70,12 @@ namespace osu.Game.Tournament.Models
 
         public readonly Bindable<DateTimeOffset> Date = new Bindable<DateTimeOffset>(DateTimeOffset.Now);
 
+        public readonly BindableInt CurrentRoundIndex = new BindableInt(1)
+        {
+            MinValue = 1,
+            MaxValue = 32,
+        };
+
         [JsonProperty]
         public readonly BindableList<ConditionalTournamentMatch> ConditionalMatches = new BindableList<ConditionalTournamentMatch>();
 
