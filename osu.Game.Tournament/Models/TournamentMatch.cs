7104c164715378ed253316193d35fe62ffd196f1
CloneWith <clonewith@qq.com>
2025-05-31 20:39:13 +0800

    Use Math.Abs for CurrentTeam calculation

--- osu.Game.Tournament/Models/TournamentMatch.cs
+++ osu.Game.Tournament/Models/TournamentMatch.cs
@@ -78,12 +78,7 @@ namespace osu.Game.Tournament.Models
             MaxValue = 17,
         };
 
-        public TeamColour CurrentTeam => CurrentRoundIndex.Value switch
-        {
-            -1 => TeamColour.Red,
-            0 => TeamColour.Blue,
-            _ => CurrentRoundIndex.Value % 2 == 1 ? TeamColour.Red : TeamColour.Blue,
-        };
+        public TeamColour CurrentTeam => Math.Abs(CurrentRoundIndex.Value) % 2 == 1 ? TeamColour.Red : TeamColour.Blue;
 
         [JsonProperty]
         public readonly BindableList<ConditionalTournamentMatch> ConditionalMatches = new BindableList<ConditionalTournamentMatch>();
