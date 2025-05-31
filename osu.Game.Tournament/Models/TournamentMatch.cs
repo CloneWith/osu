64608019850881c9c211e390009d062e846944e1
CloneWith <clonewith@qq.com>
2025-05-31 20:34:30 +0800

    Add draw and incomplete considerations for WinnerColour

--- osu.Game.Tournament/Models/TournamentMatch.cs
+++ osu.Game.Tournament/Models/TournamentMatch.cs
@@ -109,7 +109,8 @@ namespace osu.Game.Tournament.Models
         [JsonIgnore]
         public TournamentTeam? Loser => !Completed.Value ? null : Team1Score.Value > Team2Score.Value ? Team2.Value : Team1.Value;
 
-        public TeamColour WinnerColour => Winner == Team1.Value ? TeamColour.Red : TeamColour.Blue;
+        public TeamColour WinnerColour => Winner == null ? TeamColour.None
+            : Winner == Team1.Value ? TeamColour.Red : TeamColour.Blue;
 
         public int PointsToWin => Round.Value?.BestOf.Value / 2 + 1 ?? 0;
 
