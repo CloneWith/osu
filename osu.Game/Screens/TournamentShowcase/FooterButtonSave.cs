6c0d71bf1259af6c82f6e7f97980880e95021f03
CloneWith <clonewith@qq.com>
2026-05-06 21:32:19 +0800

    Use new footer buttons and notifications for showcase config

---
+++ osu.Game/Screens/TournamentShowcase/FooterButtonSave.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
+using osu.Game.Localisation;
+using osu.Game.Screens.Footer;
+
+namespace osu.Game.Screens.TournamentShowcase
+{
+    public partial class FooterButtonSave : ScreenFooterButton
+    {
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colour)
+        {
+            Icon = FontAwesome.Solid.Save;
+            Text = TournamentShowcaseStrings.SaveAction;
+            AccentColour = colour.Blue1;
+        }
+    }
+}
