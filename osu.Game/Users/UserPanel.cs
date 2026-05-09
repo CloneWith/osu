2a9152e0a46a7b7f1fce795465a5affc2354e286
CloneWith <clonewith@qq.com>
2026-05-09 21:27:05 +0800

    Remove duplicate using

--- osu.Game/Users/UserPanel.cs
+++ osu.Game/Users/UserPanel.cs
@@ -31,7 +31,6 @@ using osu.Game.Screens.Play;
 using osu.Game.Users.Drawables;
 using osuTK;
 using osu.Framework.Bindables;
-using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics.Sprites;
 
 namespace osu.Game.Users
