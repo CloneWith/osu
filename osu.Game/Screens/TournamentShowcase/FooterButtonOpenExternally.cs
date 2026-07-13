a144a8bb625744ec3fd0f31e02d7ae006b29f816
CloneWith <clonewith@qq.com>
2026-07-13 23:24:44 +0800

    Add "Open Externally" and "Refresh" footer buttons

---
+++ osu.Game/Screens/TournamentShowcase/FooterButtonOpenExternally.cs
@@ -0,0 +1,19 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
+using osu.Game.Localisation;
+using osu.Game.Screens.Footer;
+
+namespace osu.Game.Screens.TournamentShowcase
+{
+    public partial class FooterButtonOpenExternally : ScreenFooterButton
+    {
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colour)
+        {
+            Icon = FontAwesome.Solid.FolderOpen;
+            Text = EditorStrings.EditExternally;
+            AccentColour = colour.Orange1;
+        }
+    }
+}
---
+++ osu.Game/Screens/TournamentShowcase/FooterButtonRefresh.cs
@@ -0,0 +1,19 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
+using osu.Game.Screens.Footer;
+using osu.Game.Tournament.Localisation;
+
+namespace osu.Game.Screens.TournamentShowcase
+{
+    public partial class FooterButtonRefresh : ScreenFooterButton
+    {
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colour)
+        {
+            Icon = FontAwesome.Solid.Sync;
+            Text = BaseStrings.Refresh;
+            AccentColour = colour.Lime1;
+        }
+    }
+}
