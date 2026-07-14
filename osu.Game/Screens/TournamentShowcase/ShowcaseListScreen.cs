b85420bc385192e1ca89029977333a19851e3870
CloneWith <clonewith@qq.com>
2026-07-14 19:25:38 +0800

    Use showcase profile listing wrapper drawable

--- osu.Game/Screens/TournamentShowcase/ShowcaseListScreen.cs
+++ osu.Game/Screens/TournamentShowcase/ShowcaseListScreen.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Input.Events;
 using osu.Framework.Localisation;
 using osu.Framework.Logging;
 using osu.Framework.Screens;
-using osu.Game.Graphics.Containers;
+using osu.Framework.Threading;
 using osu.Game.Graphics.Cursor;
+using osu.Game.Graphics.UserInterface;
 using osu.Game.Localisation;
 using osu.Game.Screens.Footer;
 using osu.Game.Screens.OnlinePlay;
-using osuTK;
 
 namespace osu.Game.Screens.TournamentShowcase
 {
-    // TODO: Refreshing and other utilities
     public partial class ShowcaseListScreen : OsuScreen, ISubScreenWithTitle
     {
         public string ShortTitle => @"Profiles";
@@ -27,25 +30,22 @@ namespace osu.Game.Screens.TournamentShowcase
         [Resolved]
         private ShowcaseStorage storage { get; set; } = null!;
 
-        private readonly FillFlowContainer profileListFlow;
+        private readonly Bindable<string?> filter = new Bindable<string?>();
+
+        private readonly ShowcaseProfileListing profileListing;
+        private readonly PopoverContainer popoverContainer;
+        private readonly SearchTextBox searchTextBox;
 
         public ShowcaseListScreen()
         {
+            Masking = true;
+
             InternalChildren = new Drawable[]
             {
-                new OsuScrollContainer(Direction.Vertical)
+                popoverContainer = new PopoverContainer
                 {
                     RelativeSizeAxes = Axes.Both,
-                    Masking = false,
-                    Anchor = Anchor.TopCentre,
-                    Origin = Anchor.TopCentre,
-                    Width = 0.8f,
-                    ScrollbarOverlapsContent = false,
-                    Padding = new MarginPadding
-                    {
-                        Top = Header.HEIGHT + 20,
-                        Right = 5,
-                    },
+                    Padding = new MarginPadding { Top = Header.HEIGHT + 20 },
                     Child = new OsuContextMenuContainer
                     {
                         RelativeSizeAxes = Axes.X,
@@ -53,28 +53,59 @@ namespace osu.Game.Screens.TournamentShowcase
                         Children = new Drawable[]
                         {
                             // TODO: Add profile creation card
-                            profileListFlow = new FillFlowContainer
+                            profileListing = new ShowcaseProfileListing
                             {
                                 RelativeSizeAxes = Axes.X,
-                                AutoSizeAxes = Axes.Y,
-                                Direction = FillDirection.Vertical,
-                                Spacing = new Vector2(10),
+                                FilterString = { BindTarget = filter },
                             },
                         },
-                    }
+                    },
+                },
+                new Container
+                {
+                    Name = @"Search area",
+                    RelativeSizeAxes = Axes.X,
+                    Height = Header.HEIGHT,
+                    Child = searchTextBox = new BasicSearchTextBox
+                    {
+                        Anchor = Anchor.CentreRight,
+                        Origin = Anchor.CentreRight,
+                        RelativeSizeAxes = Axes.X,
+                        Width = 0.6f,
+                    },
                 },
             };
         }
 
-        [BackgroundDependencyLoader]
-        private void load()
+        protected override void LoadComplete()
         {
+            base.LoadComplete();
+
+            searchTextBox.Current.BindValueChanged(_ => updateFilterDebounced());
+
+            updateFilter();
             reloadProfiles();
+        }
+
+        public void UpdateFilter() => Scheduler.AddOnce(updateFilter);
+
+        private ScheduledDelegate? scheduledFilterUpdate;
+
+        private void updateFilterDebounced()
+        {
+            scheduledFilterUpdate?.Cancel();
+            scheduledFilterUpdate = Scheduler.AddDelayed(UpdateFilter, 200);
+        }
+
+        private void updateFilter()
+        {
+            scheduledFilterUpdate?.Cancel();
+            filter.Value = searchTextBox.Current.Value;
         }
 
         private void reloadProfiles()
         {
-            profileListFlow.Clear();
+            profileListing.Profiles.Clear();
 
             var results = storage.ListTournaments();
 
@@ -86,11 +117,7 @@ namespace osu.Game.Screens.TournamentShowcase
 
                     if (config != null)
                     {
-                        profileListFlow.Add(new ShowcaseProfileItem(config)
-                        {
-                            // TODO: Add more actions
-                            OnEdit = () => this.Push(new ShowcaseConfigScreen(config)),
-                        });
+                        profileListing.Profiles.Add(config);
                     }
                 }
                 catch (Exception e)
@@ -112,12 +139,49 @@ namespace osu.Game.Screens.TournamentShowcase
             },
         };
 
+        public override void OnEntering(ScreenTransitionEvent e)
+        {
+            base.OnEntering(e);
+            profileListing.FadeIn(300, Easing.OutQuint);
+        }
+
+        public override bool OnExiting(ScreenExitEvent e)
+        {
+            onLeaving();
+            return base.OnExiting(e);
+        }
+
+        public override void OnSuspending(ScreenTransitionEvent e)
+        {
+            onLeaving();
+            base.OnSuspending(e);
+            profileListing.FadeOut(300, Easing.OutQuint);
+        }
+
         public override void OnResuming(ScreenTransitionEvent e)
         {
             base.OnResuming(e);
+            onReturning();
+            profileListing.FadeIn(300, Easing.OutQuint);
 
             // After navigating back to this screen, the file might have been updated.
             reloadProfiles();
+        }
+
+        protected override void OnFocus(FocusEvent e)
+        {
+            searchTextBox.TakeFocus();
+        }
+
+        private void onReturning()
+        {
+            searchTextBox.HoldFocus = true;
+        }
+
+        private void onLeaving()
+        {
+            searchTextBox.HoldFocus = false;
+            popoverContainer.HidePopover();
         }
     }
 }
